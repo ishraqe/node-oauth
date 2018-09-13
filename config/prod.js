5b0876b06cc65442f7ee78b370b130d4e187b7c7
ishraqe <kanak431@diu.edu.bd>
2018-09-13 16:12:27 +0600

    environment done

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoUri: process.env.MONGO_URI,
+  cookieKeys: process.env.COOKIE_KEYS
+};
